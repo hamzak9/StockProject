75a5067c2125859eca54f0db9895fb0658f811f6
JustinSo1 <sojustin28@gmail.com>
2023-08-18 12:07:58 -0400

    Add portfolio component with placeholder data

--- frontend/src/components/portfolio.js
+++ frontend/src/components/portfolio.js
@@ -1,7 +1,28 @@
-import { Box, Text } from "@chakra-ui/react";
-import { Flex, Spacer } from "@chakra-ui/react";
+import { Box, Flex, Heading, Spacer, Text } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const baseURL = "http://localhost:8080/api/compactdiscs";
 
 export default function PortfolioPerformance() {
+  const [portfolio, setPortfolio] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function getPost() {
+      try {
+        const response = await axios.get(baseURL);
+        setPortfolio(response.data);
+      } catch (error) {
+        setError(error);
+      }
+    }
+    getPost();
+  }, []);
+
+  if (error) return `Error: ${error.message}`;
+  if (!portfolio) return "No post!";
+
   return (
     <Box>
       <Flex>
@@ -13,6 +34,15 @@ export default function PortfolioPerformance() {
           <Text fontSize="5xl">$1000.00</Text>
         </Box>
       </Flex>
+      <Box>
+        {portfolio.map((item) => (
+          <Box key={item.id} p="4">
+            <Heading size="md">{item.title}</Heading>
+            <Text mt="2">{item.artist}</Text>
+            <Text>${item.price}</Text>
+          </Box>
+        ))}
+      </Box>
     </Box>
   );
 }
