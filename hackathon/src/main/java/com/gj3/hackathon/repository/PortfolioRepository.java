6a7e42dc731e6498319a3723eaffa19b4287a9ed
Daniel <danielkim0618@gmail.com>
2023-08-24 16:18:59 -0600

    updated portfolio repository

--- hackathon/src/main/java/com/gj3/hackathon/repository/PortfolioRepository.java
+++ hackathon/src/main/java/com/gj3/hackathon/repository/PortfolioRepository.java
@@ -1,4 +1,11 @@
 package com.gj3.hackathon.repository;
 
-public class PortfolioRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gj3.hackathon.entities.Portfolio;
+//import org.springframework.data.repository.RepositoryDefinition;
+
+@Repository
+public interface PortfolioRepository extends CrudRepository<Portfolio, Integer> {
 }
