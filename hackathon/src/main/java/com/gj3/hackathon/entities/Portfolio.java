fcc3ed6d112bf59c57e733e28f53cb6075f49d49
Daniel <danielkim0618@gmail.com>
2023-08-25 14:50:43 -0600

    updated hashmap in portfolio entity

--- hackathon/src/main/java/com/gj3/hackathon/entities/Portfolio.java
+++ hackathon/src/main/java/com/gj3/hackathon/entities/Portfolio.java
@@ -8,37 +8,50 @@ import java.util.HashMap;
 //@Entity
 //@Table(name="Portfolio")
 public class Portfolio implements Serializable {
-//
-//    @Id
-//    @GeneratedValue(strategy= GenerationType.IDENTITY)
-//    @Column(name="id")
-//    private Integer id;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+    @Column(name="ticker")
+    private String ticker;
+
+    @Column(name="amount")
+    private Integer amount;
 //    private HashMap<Stock,Integer> portfolio = new HashMap<>();
 //    // { AAPL : 5, TSLA : 12 ... }
-//
-//    public Portfolio(Integer id, HashMap<Stock, Integer> portfolio) {
-//        this.id = id;
-//        this.portfolio = portfolio;
-//    }
-//
-//    public Portfolio() {
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public HashMap<Stock, Integer> getPortfolio() {
-//        return portfolio;
-//    }
-//
-//    public void setPortfolio(HashMap<Stock, Integer> portfolio) {
-//        this.portfolio = portfolio;
-//    }
-//
 
+
+    public Portfolio(Integer id, String ticker, Integer amount) {
+        this.id = id;
+        this.ticker = ticker;
+        this.amount = amount;
+    }
+
+    public Portfolio() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
 }
