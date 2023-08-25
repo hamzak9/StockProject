a6d5d86ee4676302e461bc2377c9e6c2d737b559
Hamza Khan <hamza_1456@hotmail.com>
2023-08-25 14:37:07 -0400

    Setup connection to database

--- hackathon/src/main/java/com/gj3/hackathon/entities/Portfolio.java
+++ hackathon/src/main/java/com/gj3/hackathon/entities/Portfolio.java
@@ -5,40 +5,40 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.HashMap;
-@Entity
-@Table(name="Portfolio")
+//@Entity
+//@Table(name="Portfolio")
 public class Portfolio implements Serializable {
-
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="id")
-    private Integer id;
-    private HashMap<Stock,Integer> portfolio = new HashMap<>();
-    // { AAPL : 5, TSLA : 12 ... }
-
-    public Portfolio(Integer id, HashMap<Stock, Integer> portfolio) {
-        this.id = id;
-        this.portfolio = portfolio;
-    }
-
-    public Portfolio() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public HashMap<Stock, Integer> getPortfolio() {
-        return portfolio;
-    }
-
-    public void setPortfolio(HashMap<Stock, Integer> portfolio) {
-        this.portfolio = portfolio;
-    }
-
+//
+//    @Id
+//    @GeneratedValue(strategy= GenerationType.IDENTITY)
+//    @Column(name="id")
+//    private Integer id;
+//    private HashMap<Stock,Integer> portfolio = new HashMap<>();
+//    // { AAPL : 5, TSLA : 12 ... }
+//
+//    public Portfolio(Integer id, HashMap<Stock, Integer> portfolio) {
+//        this.id = id;
+//        this.portfolio = portfolio;
+//    }
+//
+//    public Portfolio() {
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public HashMap<Stock, Integer> getPortfolio() {
+//        return portfolio;
+//    }
+//
+//    public void setPortfolio(HashMap<Stock, Integer> portfolio) {
+//        this.portfolio = portfolio;
+//    }
+//
 
 }
--- hackathon/src/main/java/com/gj3/hackathon/repository/PortfolioRepository.java
+++ hackathon/src/main/java/com/gj3/hackathon/repository/PortfolioRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.gj3.hackathon.entities.Portfolio;
 //import org.springframework.data.repository.RepositoryDefinition;
 
-@Repository
-public interface PortfolioRepository extends CrudRepository<Portfolio, Integer> {
-}
+//@Repository
+//public interface PortfolioRepository extends CrudRepository<Portfolio, Integer> {
+//}
