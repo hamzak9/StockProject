27875e1c566db8f5fbd20f18188568c819944a02
boho <bhkimca@gmail.com>
2023-08-30 15:49:02 -0400

    cash deposit & withdraw button

--- frontend/src/components/portfolio/UserBalance.js
+++ frontend/src/components/portfolio/UserBalance.js
@@ -3,9 +3,11 @@ import classes from "./UserBalance.module.css";
 import axios from "axios";
 
 const UserBalance = () => {
-  const [cashBalance, setCashBalance] = useState(0);
-  const [totalStockAmount, setTotalStockAmount] = useState(0);
-  const [totalRateOfReturn, setTotalRateOfReturn] = useState(0);
+  const [cashBalance, setCashBalance] = useState("");
+  const [totalStockAmount, setTotalStockAmount] = useState("");
+  const [totalRateOfReturn, setTotalRateOfReturn] = useState("");
+  const [cashInput, setCashInput] = useState("");
+
   useEffect(() => {
     axios
       .get("http://localhost:8080/all stock price * quantity sum api url")
@@ -30,6 +32,36 @@ const UserBalance = () => {
         setTotalRateOfReturn(response.data);
       });
   }, []);
+
+  const depositHandler = () => {
+    if (+cashInput !== null && +cashInput !== undefined && +cashInput > 0) {
+      let url = "http://localhost:8080/post cash deposit";
+      const data = { amount: cashInput };
+      axios.post(url, data);
+    } else {
+    }
+    setCashInput("");
+  };
+
+  const withdrawHandler = () => {
+    if (
+      +cashInput !== null &&
+      +cashInput !== undefined &&
+      +cashInput > 0 &&
+      +cashBalance >= +cashInput
+    ) {
+      let url = "http://localhost:8080/post cash withdraw";
+      const data = { amount: cashInput };
+      axios.post(url, data);
+    }
+    setCashInput("");
+  };
+
+  const cashNumHandler = (event) => {
+    event.preventDefault();
+    setCashInput(event.target.value);
+  };
+
   return (
     <section className={classes.balance}>
       <div>UserName</div>
@@ -44,8 +76,11 @@ const UserBalance = () => {
         Current total net:
         <section> ${+totalStockAmount + +cashBalance}</section>
       </div>
-      <button>Deposit Cash</button>
-      <button>Withdraw Cash</button>
+      <form>
+        <input type="number" onChange={cashNumHandler} />
+        <button onClick={depositHandler}>Deposit</button>
+        <button onClick={withdrawHandler}>Withdraw</button>
+      </form>
     </section>
   );
 };
