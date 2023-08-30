ed42a231d9312bdff1f580102d39a69734f5d4fc
boho <bhkimca@gmail.com>
2023-08-30 16:13:45 -0400

    temporay removing graphs

--- frontend/src/components/trends/ShowStockInfo.js
+++ frontend/src/components/trends/ShowStockInfo.js
@@ -2,11 +2,7 @@ import classes from "./ShowStokInfo.module.css";
 import PortfolioPerformance from "../portfolio";
 
 const ShowStockInfo = () => {
-  return (
-    <section className={classes.stockinfo}>
-      <PortfolioPerformance />
-    </section>
-  );
+  return <section className={classes.stockinfo}></section>;
 };
 
 export default ShowStockInfo;
