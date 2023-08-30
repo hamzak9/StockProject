a26728f774009d49b51b52fee022d941be820811
alexander-achcar <achcar.alexander@gmail.com>
2023-08-30 16:05:50 -0400

    Backend Access for OrderHistory
    Added functionality to get order history from database and return from api/order/getOrderHistory

--- hackathon/src/main/java/com/gj3/hackathon/repository/OrderRepository.java
+++ hackathon/src/main/java/com/gj3/hackathon/repository/OrderRepository.java
@@ -1,11 +1,19 @@
 package com.gj3.hackathon.repository;
 
 import com.gj3.hackathon.entities.Order;
+import com.gj3.hackathon.entities.Stock;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Integer> {
 
+    @Query("SELECT s FROM Stock s WHERE s.ticker =:ticker")
+    Optional<Stock> getAllOrders(@Param("ticker") String ticker);
 
 }
