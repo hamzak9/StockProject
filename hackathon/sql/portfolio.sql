263b059f3479d17fdc4b363c0f9d29da5d5ffd1b
Daniel <danielkim0618@gmail.com>
2023-08-31 14:02:52 -0600

    added api user endpoints

--- hackathon/sql/portfolio.sql
+++ hackathon/sql/portfolio.sql
@@ -11,3 +11,11 @@ CREATE TABLE Stocks (id int primary key auto_increment, price double, ticker var
 --/*insert into stocks values(9, 259.16, 'TSLA');
 --insert into stocks values(10, 174.52, 'APPL');
 --insert into stocks values(11, 316.49, 'MSFT');*/
+
+insert into users values(2, 230.00);
+insert into users values(3, 1230.00);
+insert into users values(4, 937.11);
+insert into users values(5, 1000000.23);
+insert into users values(6, 13.99);
+insert into users values(7, 81110.10);
+insert into users values(8, 123.31);
--- hackathon/src/main/java/com/gj3/hackathon/controller/UserController.java
+++ hackathon/src/main/java/com/gj3/hackathon/controller/UserController.java
@@ -1,17 +1,18 @@
 package com.gj3.hackathon.controller;
 
+import com.gj3.hackathon.entities.Stock;
+import com.gj3.hackathon.services.StocksService;
 import com.gj3.hackathon.services.UserService;
+import io.swagger.models.Response;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/portfolio")
@@ -20,6 +21,9 @@ public class UserController {
 
     @Autowired
     public UserService userService;
+
+    @Autowired
+    public StocksService stockService;
 
     @PostMapping("/depositCash")
     public ResponseEntity<?> depositCash(@RequestBody String payload) throws IOException, InterruptedException {
@@ -56,5 +60,41 @@ public class UserController {
         }
     }
 
+    @GetMapping("/getCashBalance")
+    public ResponseEntity<?> getCashBalance(){
+        Integer userId = 1;
 
+        Double currentCash = userService.getUserCashById(userId);
+        return new ResponseEntity<>(currentCash, HttpStatus.OK);
+    }
+
+    @GetMapping("/getTotalStocks")
+    public ResponseEntity<?> getTotalStocks() {
+        List<Stock> portfolio = stockService.getAllStocks();
+        double stocksValue = addStocks(portfolio);
+
+        return new ResponseEntity<>(stocksValue, HttpStatus.OK);
+    }
+
+    @GetMapping("/getTotalNet")
+    public ResponseEntity<?> getTotalNet() {
+        Integer userId = 1;
+        List<Stock> portfolio = stockService.getAllStocks();
+
+        double stocksValue = addStocks(portfolio);
+        Double currentCash = userService.getUserCashById(userId);
+
+        double totalNet = stocksValue + currentCash;
+        return new ResponseEntity<>(totalNet, HttpStatus.OK);
+    }
+
+    public double addStocks(List<Stock> portfolio) {
+        double stocksValue = 0.0;
+        for (Stock stock : portfolio) {
+            Double currentPrice = stock.getPrice();
+            Integer currentQuantity = stock.getQuantity();
+            stocksValue += (currentPrice * currentQuantity);
+        }
+        return stocksValue;
+    }
 }
